728f349c51cdf3020f2153038aae30d0a640dc80
KL <kim.phanhoang@epfl.ch>
2017-11-22 17:21:43 +0100

    maj

--- js/main.js
+++ js/main.js
@@ -1,114 +1,136 @@
+// js.cytoscape.org
+
+
+
 visuWidth = window.innerWidth || document.body.clientWidth;
 visuHeight = 600;
 
 fixedEdgeLength = 100;
 
-d3.csv("https://raw.githubusercontent.com/roblan11/DataVis/master/data/pokemon.csv", function(pokemon) {
 
+    d3.csv("https://raw.githubusercontent.com/roblan11/DataVis/master/data/pokemon.csv", function(pokemon) {
 
-
-    // data processing 
-/*
-    console.log(data)
-
-    let nodes = []
-    let edges = []
-
-    foreach(p) {
-        node
-        nodes.push(node)
-    }
-
-    let data = {"nodes": nodes, "edges":edges}
-    */
-
-
-    // data to test
-    data = {
+      data =  {
         "nodes": [
-        {"id":"8", "fx": visuWidth/2, "fy": visuHeight/2, "current":1 },
-        {"id":"9"},
-        {"id":"19"},
-        {"id":"11"},
-        {"id":"12"},
-        {"id":"12"},
-        {"id":"12"},
-        {"id":"12"},
-        {"id":"12"},
-        {"id":"11"},
-        {"id":"11"},
-        {"id":"11"},
-        {"id":"11"},
-        {"id":"11"},
+        {"id":"0" },
+        {"id":"12", "level":1},
+        {"id":"12", "level":1.5},
+        {"id":"11", "level":1.5},
+        {"id":"12", "level":1.5},
+        {"id":"11", "level":1.5},
+        {"id":"12", "level":1.5},
+        {"id":"11", "level":1.5},
+        {"id":"12", "level":2},
+        {"id":"12", "level":2},
+        {"id":"12", "level":2},
+        {"id":"12", "level":2},
+        {"id":"12", "level":2},
+        {"id":"11", "level":2},
+        {"id":"12", "level":2},
+        {"id":"12", "level":2},
+        {"id":"12", "level":2},
+        {"id":"11", "level":2},
+        {"id":"12", "level":2},
         ]
     }
 
-    createForceGraph(data);
-
-    function createForceGraph(data) {
-
-        d3.select("svg#main").attr("height", visuHeight);
-
-        var edges = data.edges;
-        var nodes = data.nodes;
 
 
+    var cy = cytoscape({
+        container: document.getElementById('cy'),
 
-        var colors = ["#996666", "#ff9966", "#339999", "#6699cc", "#ffcc66", "#ff6600", "#00ccccc"];
+        style: [
+        {
+            selector: 'node',
+            style: {
+                'height': 20,
+                'width': 20,
+                'background-color': '#30c9bc'
+            }
+        },
+        {
+            selector: 'node.current',
+            style: {
+                'border-color': '#000000'
+            }
+        }
 
-    //This isn't "gravity" it's the visual centering of the network based on its mass
-    var center = d3.forceCenter().x(visuWidth/2).y(visuHeight/2);
-
-    //CHARGE
-    var manyBody = d3.forceManyBody().strength(-50).distanceMin(0)
-
-
-    //Make the x-position equal to the x-position specified in the module positioning object or, if not in
-    //the hash, then set it to 250
-    var forceX = d3.forceX().strength(0.05)
-
-    //Same for forceY--these act as a gravity parameter so the different strength determines how closely
-    //the individual nodes are pulled to the center of their module position
-    var forceY = d3.forceY().strength(0.05)
-
-
-    var force = d3.forceSimulation(nodes)
-    .force("charge", manyBody)
-    .force("center", center)
-    .force("x", forceX)
-    .force("y", forceY)
-    .on("tick", updateForceGraph);
+        ]
+    });
 
 
-    var nodeEnter = d3.select("svg#main").selectAll("g.node")
-    .data(nodes)
-    .enter()
-    .append("g")
-    .attr("class", "node")
-     /*   .attr("fx", function(d){return d.id=="8" ? 130 : null})
-     .attr("fy", function(d){return d.id=="8" ? 200 : null})*/
 
-     nodeEnter.append("circle")
-     .attr("r", function(d) {return d.current ? 30 : 15 })
-     .style("fill", function (d) {return d.id%2 ? "black" : "red"})
-     .style("stroke", "black")
-
-
-    //  force.start();
-
-    function updateForceGraph(e) {
-        d3.select("svg#main").selectAll("line")
-        .attr("x1", function (d) {return d.source.x})
-        .attr("y1", function (d) {return d.source.y})
-        .attr("x2", function (d) {return d.target.x})
-        .attr("y2", function (d) {return d.target.y});
-
-        d3.select("svg#main").selectAll("g.node")
-        .attr("transform", function (d) {return "translate(" + d.x + "," + d.y + ")"});
+    var concentricOptions = {
+        name: 'concentric',
+        concentric: function(node) {
+          return 10 - node.data('level');
+      },
+      levelWidth: function() {
+          return 1;
+      },
+      animate: true
+  };
 
 
+    // init
+    function initGraph() {
+        data.nodes.forEach(function(n) {
+
+            // You have to have threshold to know the level of the pokemon in the graph
+
+            let l = 2; 
+            let stylesOptions = {}
+            let currentParam = 0
+
+            if (n.id == 0) {
+                l = 0
+                currentParam = 1
+                stylesOptions = {
+                    "background-color": "red",
+                    "width":50,
+                    "height":50
+                }
+            }
+
+            cy.add({
+                data: {
+                    level: l,
+                    current: currentParam
+                }, 
+                style : stylesOptions
+            });
+
+        });
+
+        cy.layout(concentricOptions).run();
     }
 
-}
 
-})
+
+
+    // on click event
+    cy.on('tap', 'node', function(evt){
+
+        node = evt.target;
+
+        /*old_current = cy.elements("node[current=1]")
+        console.log(old_current.data())
+
+        old_current.data("current", 0)
+        node.data("current", 1)*/
+
+        node.data("level", 0)
+
+        // update graph
+        cy.layout(concentricOptions).run();
+
+    });
+
+
+
+
+
+    initGraph()
+
+
+});
