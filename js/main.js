8b18589820afe6e1dacf87771358633a334790cd
KL <kim.phanhoang@epfl.ch>
2017-11-22 17:37:15 +0100

    maj : default and current style

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
 // js.cytoscape.org
 
 
-
 visuWidth = window.innerWidth || document.body.clientWidth;
 visuHeight = 600;
 
 fixedEdgeLength = 100;
 
 
-    d3.csv("https://raw.githubusercontent.com/roblan11/DataVis/master/data/pokemon.csv", function(pokemon) {
+d3.csv("https://raw.githubusercontent.com/roblan11/DataVis/master/data/pokemon.csv", function(pokemon) {
 
-      data =  {
+    data = {
         "nodes": [
         {"id":"0" },
         {"id":"12", "level":1},
@@ -36,25 +35,26 @@ fixedEdgeLength = 100;
 
 
 
+    let stylesOptionsDefault = {
+        'height': 20,
+        'width': 20,
+        'background-color': '#30c9bc'
+    }
+
+    let stylesOptionsCurrent = {
+        "background-color": "red",
+        "width":50,
+        "height":50
+    }
+
     var cy = cytoscape({
         container: document.getElementById('cy'),
 
         style: [
         {
             selector: 'node',
-            style: {
-                'height': 20,
-                'width': 20,
-                'background-color': '#30c9bc'
-            }
-        },
-        {
-            selector: 'node.current',
-            style: {
-                'border-color': '#000000'
-            }
+            style: stylesOptionsDefault
         }
-
         ]
     });
 
@@ -63,18 +63,18 @@ fixedEdgeLength = 100;
     var concentricOptions = {
         name: 'concentric',
         concentric: function(node) {
-          return 10 - node.data('level');
-      },
-      levelWidth: function() {
-          return 1;
-      },
-      animate: true
-  };
+            return 10 - node.data('level');
+        },
+        levelWidth: function() {
+            return 1;
+        },
+        animate: true
+    };
 
 
     // init
     function initGraph() {
-        data.nodes.forEach(function(n) {
+        data.nodes.forEach(n => {
 
             // You have to have threshold to know the level of the pokemon in the graph
 
@@ -85,11 +85,7 @@ fixedEdgeLength = 100;
             if (n.id == 0) {
                 l = 0
                 currentParam = 1
-                stylesOptions = {
-                    "background-color": "red",
-                    "width":50,
-                    "height":50
-                }
+                stylesOptions = stylesOptionsCurrent
             }
 
             cy.add({
@@ -98,11 +94,25 @@ fixedEdgeLength = 100;
                     current: currentParam
                 }, 
                 style : stylesOptions
-            });
+            })
 
-        });
+        }) 
 
         cy.layout(concentricOptions).run();
+    }
+
+
+    function updateGraph(currentNode){
+        cy.elements().toArray().forEach(n => {
+            if (n == currentNode){
+                n.data("level",0)
+                n.style(stylesOptionsCurrent)
+            }
+            else {
+                n.data("level",1)
+                n.style(stylesOptionsDefault)
+            }
+        })
     }
 
 
@@ -110,6 +120,7 @@ fixedEdgeLength = 100;
 
     // on click event
     cy.on('tap', 'node', function(evt){
+
 
         node = evt.target;
 
@@ -119,14 +130,12 @@ fixedEdgeLength = 100;
         old_current.data("current", 0)
         node.data("current", 1)*/
 
-        node.data("level", 0)
+        updateGraph(node)
 
         // update graph
         cy.layout(concentricOptions).run();
 
     });
-
-
 
 
 
